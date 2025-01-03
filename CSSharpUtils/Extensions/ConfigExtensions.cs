5a3f816cc75c10f8544e8d73c563a0c06ed89706
Oscar <29248751+oscar-wos@users.noreply.github.com>
2025-01-03 20:21:05 +0000

    feat: backups for new config verions

--- CSSharpUtils/Extensions/ConfigExtensions.cs
+++ CSSharpUtils/Extensions/ConfigExtensions.cs
@@ -15,7 +15,7 @@ public static class ConfigExtensions
 
     // Defines the path to the configuration file, based on the game directory and assembly name.
     public static readonly string ConfigPath =
-        $"{Server.GameDirectory}/csgo/addons/counterstrikesharp/configs/plugins/{AssemblyName}/{AssemblyName}.json";
+        $"{Server.GameDirectory}/csgo/addons/counterstrikesharp/configs/plugins/{AssemblyName}/{AssemblyName}";
 
     // Specifies the options for JSON serialization, including indentation for readability.
     private static readonly JsonSerializerOptions WriteSerializerOptions = new() { WriteIndented = true };
@@ -28,6 +28,7 @@ public static class ConfigExtensions
     /// Updates the version of the provided configuration object and serializes it back to JSON.
     /// This method ensures that the configuration file reflects the most recent version,
     /// including all properties of the configuration object, even those not initially set.
+    /// Also backs up the current configuration file before updating it.
     /// </summary>
     /// <typeparam name="T">The type of the configuration object, must inherit from BasePluginConfig.</typeparam>
     /// <param name="config">The configuration object to update and serialize.</param>
@@ -40,6 +41,12 @@ public static class ConfigExtensions
         // loaded config is up-to-date
         if (config.Version == newCfgVersion)
             return false;
+
+        // get counter of backup file
+        var backupCount = GetBackupCount();
+
+        // create a backup of the current config
+        File.Copy($"{ConfigPath}.json", $"{ConfigPath}-{backupCount}.bak", true);
 
         // update the version
         config.Version = newCfgVersion;
@@ -61,9 +68,19 @@ public static class ConfigExtensions
     public static T Reload<T>(this T config) where T : BasePluginConfig, new()
     {
         // read the configuration file content
-        var configContent = File.ReadAllText(ConfigPath);
+        var configContent = File.ReadAllText($"{ConfigPath}.json");
 
         // deserialize the configuration content back to the object
         return JsonSerializer.Deserialize<T>(configContent, ReadSerializerOptions)!;
+    }
+
+    private static int GetBackupCount()
+    {
+        var counter = 0;
+
+        while (File.Exists($"{ConfigPath}-{counter}.bak"))
+            counter++;
+
+        return counter;
     }
 }
