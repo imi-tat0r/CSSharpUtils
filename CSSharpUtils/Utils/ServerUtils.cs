b283811c74f1c6c238b8d5b70049def91716a7b8
oscar-wos <29248751+oscar-wos@users.noreply.github.com>
2025-06-03 06:51:00 +0100

    refactor: ServerUtils.GetServerIp

--- CSSharpUtils/Utils/ServerUtils.cs
+++ CSSharpUtils/Utils/ServerUtils.cs
@@ -1,5 +1,5 @@
-﻿using System.Runtime.InteropServices;
-using CounterStrikeSharp.API.Core;
+﻿using CounterStrikeSharp.API.Core;
+using System.Runtime.InteropServices;
 
 namespace CSSharpUtils.Utils;
 
@@ -8,32 +8,30 @@ namespace CSSharpUtils.Utils;
 /// </summary>
 public static class ServerUtils
 {
-    private delegate nint CNetworkSystem_UpdatePublicIp(nint a1);
-    private static CNetworkSystem_UpdatePublicIp? _networkSystemUpdatePublicIp;
-    private static IntPtr _networkSystem = IntPtr.Zero;
-    
+    private readonly static IntPtr _networkSystem;
+
+    private delegate nint CNetworkSystemUpdatePublicIp(IntPtr networkSystem);
+    private static CNetworkSystemUpdatePublicIp? _networkSystemUpdatePublicIp;
+
+    static unsafe ServerUtils()
+    {
+        _networkSystem = NativeAPI.GetValveInterface(0, "NetworkSystemVersion001");
+    }
+
     /// <summary>
     /// Gets the IP address of the server.
     /// </summary>
     /// <returns>The IP address of the server.</returns>
-    /// <remarks>Taken from https://github.com/daffyyyy/CS2-SimpleAdmin/blob/main/Helper.cs#L450</remarks>
-    public static string GetServerIp()
+    /// <remarks>Taken from https://discord.com/channels/1160907911501991946/1233009182857494588</remarks>
+    public unsafe static string GetServerIp()
     {
-        unsafe
+        if (_networkSystemUpdatePublicIp == null)
         {
-            if (_networkSystem == IntPtr.Zero)
-                _networkSystem = NativeAPI.GetValveInterface(0, "NetworkSystemVersion001");
-            
-            if (_networkSystemUpdatePublicIp is null)
-            {
-                var funcPtr = *(nint*)(*(nint*)(_networkSystem) + 256);
-                _networkSystemUpdatePublicIp = Marshal.GetDelegateForFunctionPointer<CNetworkSystem_UpdatePublicIp>(funcPtr);
-            }
-            
-            // + 4 to skip type, because the size of uint32_t is 4 bytes
-            var ipBytes = (byte*)(_networkSystemUpdatePublicIp(_networkSystem) + 4);
-            // port is always 0, use the one from convar "hostport"
-            return $"{ipBytes[0]}.{ipBytes[1]}.{ipBytes[2]}.{ipBytes[3]}";
+            var funcPtr = *(nint*)(*(nint*)_networkSystem + 256);
+            _networkSystemUpdatePublicIp = Marshal.GetDelegateForFunctionPointer<CNetworkSystemUpdatePublicIp>(funcPtr);
         }
+
+        var ipBytes = (byte*)(_networkSystemUpdatePublicIp(_networkSystem) + 4);
+        return $"{ipBytes[0]}.{ipBytes[1]}.{ipBytes[2]}.{ipBytes[3]}";
     }
 }
